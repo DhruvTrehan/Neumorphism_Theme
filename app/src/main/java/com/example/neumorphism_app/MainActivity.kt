d77de8478ad53a6aa92945860bd62c42507b3bac
Vivek <786vt786@gmail.com>
2022-10-08 17:04:46 +0530

    final commit

--- app/src/main/java/com/example/neumorphism_app/MainActivity.kt
+++ app/src/main/java/com/example/neumorphism_app/MainActivity.kt
@@ -18,41 +18,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tvSignup!!.setOnClickListener{  Toast.makeText(applicationContext,"Available soon",Toast.LENGTH_SHORT).show() }
-
-        binding.mnLogo.speed = 3f;
-
-        binding.mnLogo.setOnClickListener{
-
-            isCheckedDone = if(isCheckedDone){
-
-
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                Toast.makeText(applicationContext,"Shifted to Dark Mode",Toast.LENGTH_SHORT).show()
-                binding.tvApp!!.text = "Bright"
-                binding.mnLogo.setAnimation(R.raw.dark)
-                binding.mnLogo.setMinAndMaxProgress(0.5f,1.0f)
-                binding.mnLogo.playAnimation()
-                false
-
-            }else{
-
-                Toast.makeText(applicationContext,"Shifted to Bright Mode",Toast.LENGTH_SHORT).show()
-                binding.tvApp!!.text = "Dark"
-                binding.mnLogo.setAnimation(R.raw.ddark)
-                binding.mnLogo.setMinAndMaxProgress(0.0f,0.5f)
-                binding.mnLogo.playAnimation()
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                true
-
-            }
-
+        binding.tvSignup!!.setOnClickListener {
+            Toast.makeText(
+                applicationContext,
+                "Available soon",
+                Toast.LENGTH_SHORT
+            ).show()
         }
-
-
-
-
-
-
     }
 }
