555003114badcc3c01c602e93fd907ac60a53878
Vivek <786vt786@gmail.com>
2022-10-08 17:14:20 +0530

    sec commit

--- app/src/main/java/com/example/neumorphism_app/MainActivity.kt
+++ app/src/main/java/com/example/neumorphism_app/MainActivity.kt
@@ -3,15 +3,11 @@ package com.example.neumorphism_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatDelegate
-import com.airbnb.lottie.LottieAnimationView
 import com.example.neumorphism_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-    var isCheckedDone = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
